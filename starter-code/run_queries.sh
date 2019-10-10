10cfbbb8f9c42ba7307b6ae586ead9066d4c144f
medjitov10 <medjitov10@gmail.com>
2019-10-10 16:43:53 -0400

    fixed select

--- starter-code/run_queries.sh
+++ starter-code/run_queries.sh
@@ -5,20 +5,21 @@ echo '# TODO: Write queries to display:'
 
 echo '# Employees working at macys'
 psql -d company -c "
-  select * from employees e
+  select CONCAT(first_name, ' ', last_name) as full_name from employees e
   join jobs j on e.ssn = j.emp_ssn
   where j.company like 'Macy%'
   ;"
 echo '# Companies in Boston'
 psql -d company -c "
-  select * from employees e
+  select company from employees e
   join jobs j on e.ssn = j.emp_ssn
   where e.city = 'Boston'
   ;"
 echo '# Employee with the highest salary'
 psql -d company -c "
-  select * from jobs
-  group by jobs.id
+  select CONCAT(first_name, ' ', last_name) as full_name from jobs
+  join employees e on jobs.emp_ssn = e.ssn
+  group by e.ssn, jobs.id
   having salary = (
   select max(j.salary) from employees e
   join jobs j on e.ssn = j.emp_ssn
